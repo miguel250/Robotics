9996424878e8d3858b5dded319a378eae217d966
kimbesakura <kimbesakura@yahoo.com>
2012-02-18 16:45:11 -0500

    Add sensor mapping

--- src/edu/wpi/first/wpilibj/technobots/RobotMap.java
+++ src/edu/wpi/first/wpilibj/technobots/RobotMap.java
@@ -18,4 +18,7 @@ public class RobotMap {
     public static final int shooterRight = 5,
                             shooterLeft  = 6,
                             sweeper      = 7;
+    //Sensors
+    public static final int pot         = 1,
+                            rangeFinder = 2;
 }
