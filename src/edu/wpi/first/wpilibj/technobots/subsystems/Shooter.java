9b5847d82a85248edd6c4c6f10c1f04a7c6cc5b1
Kim Nguyen <kimnguyen984@gmail.com>
2012-02-19 20:43:16 -0500

    Add Shooter

---
+++ src/edu/wpi/first/wpilibj/technobots/subsystems/Shooter.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.technobots.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author Admin
+ */
+public class Shooter extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
